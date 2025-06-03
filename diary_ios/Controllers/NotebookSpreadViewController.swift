067f780ef4dec83288b940e50265e4a0a6aaceea
Dan <danceui@outlook.com>
2025-06-03 11:48:50 +0200

    works well

--- diary_ios/Controllers/NotebookSpreadViewController.swift
+++ diary_ios/Controllers/NotebookSpreadViewController.swift
@@ -98,7 +98,8 @@ class NotebookSpreadViewController: UIViewController {
         let rightPage = NotebookPageViewController(pageIndex: insertIndex + 1, initialData: initialData)
         pages.insert(contentsOf: [leftPage, rightPage], at: insertIndex)
         print("📄 Add page pair \(insertIndex), \(insertIndex + 1).")
-        flipController.complete(direction: .nextPage, progress: 0.0)
+        flipController.begin(direction: .nextPage)
+        flipController.complete(direction: .nextPage, progress: -0.1)
     }
 
     func goToPagePair(to index: Int) {
