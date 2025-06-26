7aabbf916cdb7a161bd367b2bc1b9ada70ff1846
Dan <danceui@outlook.com>
2025-06-26 16:21:03 +0200

    corrected life cycles

--- diary_ios/UIKitBridge/NotebookViewContainer.swift
+++ diary_ios/UIKitBridge/NotebookViewContainer.swift
@@ -2,11 +2,8 @@ import SwiftUI
 
 @available(iOS 16.0, *)
 struct NotebookViewContainer: UIViewControllerRepresentable {
-    let notebookSpreadVC: NotebookSpreadViewController
-    // var width: CGFloat = 842   // 默认值
-    // var height: CGFloat = 595 // 默认值
-    // var borderColor: Color = .red // 边框颜色
-
+    let notebookSpreadVC: notebookSpreadViewController
+    
     func makeUIViewController(context: Context) -> NotebookZoomableViewController {
         return NotebookZoomableViewController(notebookSpreadViewController: notebookSpreadVC)
     }
