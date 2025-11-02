296ee8dcc81cc9b2213c1d0629fba8d882b38c03
Dan <danceui@outlook.com>
2025-11-02 20:49:51 +0100

    ready to adjust layout

--- diary_ios/Common/ToolbarCommon/StructsForToolbar.swift
+++ diary_ios/Common/ToolbarCommon/StructsForToolbar.swift
@@ -21,6 +21,7 @@ private let colorPickerSpacing = DetailsPanelConstants.colorPickerSpacing
 
 private let debugBorder = Debuggers.debugBorder
 
+// MARK: - Details Panel
 @available(iOS 26.0, *)
 struct FancyBrushPreview: View {
     let tool: Tool
@@ -63,84 +64,6 @@ struct FancyBrushPreview: View {
     }
 }
 
-// MARK: - Effect For Details Panel
-struct VerticalEdgeFade: View {
-    var fade: CGFloat
-
-    var body: some View {
-        VStack(spacing: 0) {
-            LinearGradient(
-                colors: [Color.black.opacity(0), Color.black.opacity(0.383), Color.black.opacity(0.707), Color.black.opacity(0.924), Color.black],
-                startPoint: .top, endPoint: .bottom
-            )
-            .frame(height: fade)
-
-            Rectangle().fill(.black)
-
-            LinearGradient(
-                colors: [Color.black, Color.black.opacity(0.924), Color.black.opacity(0.707), Color.black.opacity(0.383), Color.black.opacity(0)],
-                startPoint: .top, endPoint: .bottom
-            )
-            .frame(height: fade)
-        }
-    }
-}
-
-struct HorizontalEdgeFade: View {
-    var fade: CGFloat
-
-    var body: some View {
-        HStack(spacing: 0) {
-            LinearGradient(
-                colors: [Color.black.opacity(0), Color.black.opacity(0.383), Color.black.opacity(0.707), Color.black.opacity(0.924), Color.black],
-                startPoint: .leading, endPoint: .trailing
-            )
-            .frame(width: fade)
-
-            Rectangle().fill(.black)
-
-            LinearGradient(
-                colors: [Color.black, Color.black.opacity(0.924), Color.black.opacity(0.707), Color.black.opacity(0.383), Color.black.opacity(0)],
-                startPoint: .leading, endPoint: .trailing
-            )
-            .frame(width: fade)
-        }
-    }
-}
-
-struct CheckerboardBackground: View {
-    var squareSize: CGFloat
-
-    var body: some View {
-        GeometryReader { geo in
-            let cols = Int(geo.size.width / squareSize)
-            let rows = Int(geo.size.height / squareSize)
-            Canvas { context, size in
-                for y in 0..<rows {
-                    for x in 0..<cols {
-                        let isEven = (x + y).isMultiple(of: 2)
-                        let color = isEven
-                            ? Color(.systemGray5).opacity(0.6)
-                            : Color(.systemGray6).opacity(0.6)
-                        context.fill(
-                            Path(
-                                CGRect(
-                                    x: CGFloat(x) * squareSize,
-                                    y: CGFloat(y) * squareSize,
-                                    width: squareSize,
-                                    height: squareSize
-                                )
-                            ),
-                            with: .color(color)
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-// MARK: - Details Panel
 @available(iOS 26.0, *)
 struct StyleDetailsPreview: View {
     let tool: Tool
@@ -277,5 +200,74 @@ struct PresetAnchorKey: PreferenceKey {
     static var defaultValue: [PresetID: Anchor<CGRect>] = [:]
     static func reduce(value: inout [PresetID: Anchor<CGRect>], nextValue: () -> [PresetID: Anchor<CGRect>]) {
         value.merge(nextValue(), uniquingKeysWith: { $1 })
+    }
+}
+
+// MARK: - Edge Effect
+struct VerticalEdgeFade: View {
+    var fade: CGFloat
+
+    var body: some View {
+        VStack(spacing: 0) {
+            LinearGradient(
+                colors: [Color.black.opacity(0), Color.black.opacity(0.383), Color.black.opacity(0.707), Color.black.opacity(0.924), Color.black],
+                startPoint: .top, endPoint: .bottom
+            ).frame(height: fade)
+            Rectangle().fill(.black)
+            LinearGradient(
+                colors: [Color.black, Color.black.opacity(0.924), Color.black.opacity(0.707), Color.black.opacity(0.383), Color.black.opacity(0)],
+                startPoint: .top, endPoint: .bottom
+            ).frame(height: fade)
+        }
+    }
+}
+
+struct HorizontalEdgeFade: View {
+    var fade: CGFloat
+
+    var body: some View {
+        HStack(spacing: 0) {
+            LinearGradient(
+                colors: [Color.black.opacity(0), Color.black.opacity(0.383), Color.black.opacity(0.707), Color.black.opacity(0.924), Color.black],
+                startPoint: .leading, endPoint: .trailing
+            ).frame(width: fade)
+            Rectangle().fill(.black)
+            LinearGradient(
+                colors: [Color.black, Color.black.opacity(0.924), Color.black.opacity(0.707), Color.black.opacity(0.383), Color.black.opacity(0)],
+                startPoint: .leading, endPoint: .trailing
+            ).frame(width: fade)
+        }
+    }
+}
+
+struct CheckerboardBackground: View {
+    var squareSize: CGFloat
+
+    var body: some View {
+        GeometryReader { geo in
+            let cols = Int(geo.size.width / squareSize)
+            let rows = Int(geo.size.height / squareSize)
+            Canvas { context, size in
+                for y in 0..<rows {
+                    for x in 0..<cols {
+                        let isEven = (x + y).isMultiple(of: 2)
+                        let color = isEven
+                            ? Color(.systemGray5).opacity(0.6)
+                            : Color(.systemGray6).opacity(0.6)
+                        context.fill(
+                            Path(
+                                CGRect(
+                                    x: CGFloat(x) * squareSize,
+                                    y: CGFloat(y) * squareSize,
+                                    width: squareSize,
+                                    height: squareSize
+                                )
+                            ),
+                            with: .color(color)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
